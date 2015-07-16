7be38c889d7dfc39b89c5d122c2674d52ee22f00
Steve Guidetti <sguidetti@ultramegasoft.com>
2015-07-15 21:33:14 -0700

    implement tekkit query code

--- games/tekkit.class.php
+++ games/tekkit.class.php
@@ -33,7 +33,71 @@ class Game_Tekkit extends Gameserver {
 
     protected $port = 25565;
 
-    public function query() {
+    /**
+     * Pack a string into packet format
+     * 
+     * @param string $string
+     * @return string
+     */
+    protected static function packString($string) {
+        $len = strlen($string);
+        return pack('n', $len) . mb_convert_encoding($string, 'UTF-16BE');
+    }
 
+    /**
+     * Decode a UTF-16BE string
+     * 
+     * @param string $string UTF-16BE string
+     * @return string UTF-8 string
+     */
+    protected static function decodeString($string) {
+        return mb_convert_encoding($string, 'UTF-8', 'UTF-16BE');
+    }
+
+    public function query() {
+        $fp = stream_socket_client('tcp://' . $this->getAddress(), $errno, $errstr);
+        if(!$fp) {
+            throw new Exception($errstr, $errno);
+        }
+
+        stream_set_timeout($fp, 5);
+
+        $this->makeRequest($fp);
+        $this->readResponse($fp);
+    }
+
+    /**
+     * Send a Server List Ping request to the server
+     * 
+     * @param resource $fp Handle to an open socket
+     */
+    protected function makeRequest($fp) {
+        $req = pack('nc', 0xFE01, 0xFA);
+        $req .= self::packString('MC|PingHost');
+        $req .= pack('nc', 7 + 2 * strlen($this->hostname), 73);
+        $req .= self::packString($this->hostname);
+        $req .= pack('N', $this->port);
+
+        fwrite($fp, $req);
+    }
+
+    /**
+     * Read and parse the response from the server
+     * 
+     * @param resource $fp Handle to an open socket
+     * @throws Exception
+     */
+    protected function readResponse($fp) {
+        $res = fread($fp, 3);
+        if(strpos($res, 0xFF) !== 0) {
+            throw new Exception('Invalid ping response');
+        }
+
+        $res = fread($fp, 2048);
+        $data = explode(pack('n', 0), $res);
+
+        $this->setName(self::decodeString($data[3]));
+        $this->setPlayerCount((int)self::decodeString($data[4]));
+        $this->setMaxPlayers((int)self::decodeString($data[5]));
     }
 }
