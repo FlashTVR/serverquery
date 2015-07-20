a58316de67f8fe1e84320a9a381123f1aa512efe
Steve Guidetti <sguidetti@ultramegasoft.com>
2015-07-20 16:53:27 -0700

    create initial main application structure

---
+++ lib/ServerQuery.class.php
@@ -0,0 +1,85 @@
+<?php
+
+/*
+ * The MIT License
+ *
+ * Copyright 2015 Steve Guidetti.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+require 'lib/Gameserver.class.php';
+
+/**
+ * Main application class
+ *
+ * @author Steve Guidetti
+ */
+class ServerQuery {
+
+    /**
+     * Execute main application logic
+     */
+    public function exec() {
+        foreach(SQConfig::$servers as $server) {
+            $o = self::getServerObject($server);
+            try {
+                $o->query();
+            } catch (Exception $e) {
+                echo 'Error: ' . $e->getMessage() . PHP_EOL;
+            }
+            
+            var_dump($o);
+        }
+    }
+
+    /**
+     * Get a Gameserver object based on a server config
+     * 
+     * @param array $server Element from the servers config
+     * @return Gameserver
+     */
+    private static function getServerObject(array $server) {
+        $className = SQConfig::$games[$server['game']]['class'];
+        if(!class_exists($className)) {
+            $fileName = 'games/';
+            $fileName .= substr($className, strrpos($className, '_') + 1);
+            $fileName .= '.class.php';
+            require $fileName;
+        }
+
+        return new $className($server['addr'], self::getServerConfig($server));
+    }
+
+    /**
+     * Get the combined server config array
+     * 
+     * @param array $server Element from the servers config
+     * @return array
+     */
+    private static function getServerConfig(array $server) {
+        $config = array_key_exists('config', $server) ? $server['config'] : array();
+        
+        if(array_key_exists('config', SQConfig::$games[$server['game']])) {
+            $config = array_merge(SQConfig::$games[$server['game']]['config'], $config);
+        }
+        
+        return $config;
+    }
+}
